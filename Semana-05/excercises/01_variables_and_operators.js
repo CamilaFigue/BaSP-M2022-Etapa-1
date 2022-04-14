0d91e450e848c9a3ea414b2d84331b24b07271ba
camila figueroa <camilafigue2@gmail.com>
2022-04-14 19:11:11 -0300

    BaSP-Week05-2022: ex01-variables-and-operators

---
+++ Semana-05/excercises/01_variables_and_operators.js
@@ -0,0 +1,25 @@
+// Variables and operators
+
+// excercise a
+
+var num1 = 10;
+var num2 = 27;
+var resultado = num1 + num2;
+
+console.log ("ex01-variables and operators- A: La suma es:", resultado);
+
+// excercise b
+
+var carcter1 = "Hola";
+var carcter2 = "Mundo";
+var concatenacion = carcter1 + " " + carcter2;
+
+console.log ("ex01-variables and operators- B: La concatenacion es: " + concatenacion);
+
+// excercise c
+
+var text1 = "Camila";
+var text2 = "Figueroa";
+var sumaString = text1.length + text2.length;
+
+console.log ("ex01-variables and operators- C: La suma de los string de cada palabra es: " + sumaString);
